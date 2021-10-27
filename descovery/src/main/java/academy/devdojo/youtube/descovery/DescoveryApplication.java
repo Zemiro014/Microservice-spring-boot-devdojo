1944463605a9c2a5dcea16b1e8f791d34c578353
Zemiro014 <zemiro014@gmail.com>
2021-10-26 21:27:50 -0300

    Discover Service e Gateway Service

---
+++ descovery/src/main/java/academy/devdojo/youtube/descovery/DescoveryApplication.java
@@ -0,0 +1,15 @@
+package academy.devdojo.youtube.descovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DescoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescoveryApplication.class, args);
+	}
+
+}
